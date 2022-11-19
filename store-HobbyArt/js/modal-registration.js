1ed3aa25d54fb432db277425c548c2968a875646
AnnaKhiz <kukurika.anna@gmail.com>
2022-11-19 18:37:57 +0200

    login&regist forms. switch templates

--- store-HobbyArt/js/modal-registration.js
+++ store-HobbyArt/js/modal-registration.js
@@ -1,16 +1,43 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function(){
+    const modalTemplate = document.getElementById('modal');
+    const modalCloseBtn = document.getElementById('closeModal');
+    const modalCallBtn = document.getElementById('callModal');
+    const contentInModalWindow = document.getElementById('scroll-on-content');
+    const loginFormInnerContent = document.getElementById('login-inner-content');
+    const loginFormTemplate = document.getElementById('login-form-template').innerHTML;
+    const registrationFormTemplate = document.getElementById('regist-form-template').innerHTML;
+    const bodyPage = document.getElementById('body');
+    let registrationButton;
 
-    $("#callModal").on('click', (event) => {
-        $('#modal').addClass('show');
-        $('body').addClass('fixed');
-        $('.modal__registration-block').addClass('scroll');
-        event.preventDefault();
-    });
+    modalCallBtn.addEventListener('click',(e)=>{
+        modalTemplate.classList.add('show');
+        bodyPage.classList.add('fixed');
+        e.preventDefault();
+        });
 
-    $("#closeModal").on('click', (event) => {
-        $('#modal').removeClass('show');
-        $('body').removeClass('fixed');
-        $('.modal__registration-block').removeClass('scroll');
-        event.preventDefault();
+    function getElem() {
+        contentInModalWindow.innerHTML = loginFormTemplate;
+        return registrationButton = document.getElementById('regist-button');
+    }
+
+    function noName() {
+        getElem();
+        registrationButton.addEventListener('click', (e) => {
+            contentInModalWindow.innerHTML = registrationFormTemplate;
+            const loginButton = document.getElementById('login-button');
+            e.preventDefault();
+            loginButton.addEventListener('click', (e) => {
+                contentInModalWindow.innerHTML = loginFormTemplate;
+                e.preventDefault();
+                noName();
+            });
+        });
+    }
+    noName();
+
+    modalCloseBtn.addEventListener('click', (e) => {
+        modalTemplate.classList.remove('show');
+        bodyPage.classList.remove('fixed');
+        e.preventDefault();
     });
 });
--- store-HobbyArt/js/modal.js
+++ store-HobbyArt/js/modal.js
@@ -1,40 +1,40 @@
-const registrationButton = document.getElementById('regist-button');
-
-const registrationFormTemplate = document.getElementById('regist-form-template').innerHTML;
-const loginFormTemplate = document.getElementById('login-form-template').innerHTML;
-const loginFormInnerContent = document.getElementById('login-inner-content');
-const registFormInnerContent = document.getElementById('regist-inner-content');
-let logBut, regButton;
-//console.log(registrationFormTemplate);
-registrationButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    regist();
-    logBut.addEventListener('click', (e)=> {
-        login()
-        e.preventDefault();
-    });
-})
-
-function regist(){
-    console.log('hello reg')
-    loginFormInnerContent.innerHTML = registrationFormTemplate;
-    console.log(loginFormInnerContent);
-    logBut = document.getElementById('login-button');
-    console.log(logBut);
-    return logBut;
-}
-
-function login() {
-    console.log('hello login')
-    loginFormInnerContent.innerHTML = loginFormTemplate;
-    console.log(loginFormInnerContent.innerHTML);
-    regButton = document.getElementById('regist-button');
-    return regButton;
-}
-//const newloginFormInnerContent = loginFormInnerContent.innerHTML;
-
-//const loginButton = document.getElementById('login-button');
-
-
-
-
+//
+// const registrationButton = document.getElementById('regist-button');
+// const registrationFormTemplate = document.getElementById('regist-form-template').innerHTML;
+// const loginFormTemplate = document.getElementById('login-form-template').innerHTML;
+// const loginFormInnerContent = document.getElementById('login-inner-content');
+// const registFormInnerContent = document.getElementById('regist-inner-content');
+// let logBut, regButton;
+// //console.log(registrationFormTemplate);
+// registrationButton.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     regist();
+//     logBut.addEventListener('click', (e)=> {
+//         login()
+//         e.preventDefault();
+//     });
+// })
+//
+// function regist(){
+//     console.log('hello reg')
+//     loginFormInnerContent.innerHTML = registrationFormTemplate;
+//     console.log(loginFormInnerContent);
+//     logBut = document.getElementById('login-button');
+//     console.log(logBut);
+//     return logBut;
+// }
+//
+// function login() {
+//     console.log('hello login')
+//     loginFormInnerContent.innerHTML = loginFormTemplate;
+//     console.log(loginFormInnerContent.innerHTML);
+//     regButton = document.getElementById('regist-button');
+//     return regButton;
+// }
+// //const newloginFormInnerContent = loginFormInnerContent.innerHTML;
+//
+// //const loginButton = document.getElementById('login-button');
+//
+//
+//
+//
